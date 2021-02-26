c6c86c89b7f8ff9186f832ed7bd0770b9a17c4f5
Bruno Jyun <kometanijyun@gmail.com>
2021-02-25 21:54:12 -0300

    First commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+/**
+ * GET => Buscar
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração Específica
+ */
+
+import { app } from "./app";
+
+app.listen(3333, () => console.log("Server is running!"));
+
+/*
+coisas para adicionar ao projeto no começo!
+
+yarn add express
+yarn add @types/express -D
+
+yarn add typescript -D
+yarn tsc --init
+
+yarn add ts-node-dev -D
+*/
